00ab5e27378e9f96e1167756ee6367306334c9e6
tuanbui186 <11520451@gm.uit.edu.vn>
2024-11-17 16:27:09 +0700

    Add files via upload

---
+++ hellminer_win64/run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RR2irzCFVkNaZGsopir3YGkrPMWRBaPaN9.WORKER1 -p x --cpu 3
